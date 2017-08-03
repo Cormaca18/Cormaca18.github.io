1887fdd6cc3a392fa94728f3589b4bc329341157
Cormac Ahern <Cormac Ahern>
2017-08-03 22:28:24 +0100

    changed CV file and html

--- final/about.js
+++ final/about.js
@@ -17,7 +17,8 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 1; i <= slides.length; i++) {
-      slides[i].style.display = "none";
+    window.getComputedStyle(slides[i]).display="none";
+      //slides[i].style.display = "none";
   }
   for (i = 2; i <= dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
